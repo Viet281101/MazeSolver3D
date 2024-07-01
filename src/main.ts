bf925766c2ceeb6143c1a377e7c3b4ef62f5e49d
Viet281101 <nva281101@gmail.com>
2024-07-01 06:35:15 +0200

    update style

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import './style.css';
 import { initMaze } from './maze';
 
 window.onload = () => {
-    const canvas = document.getElementById('mazeCanvas') as HTMLCanvasElement;
-    if (canvas) {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        initMaze(canvas);
-    }
+	const canvas = document.getElementById('mazeCanvas') as HTMLCanvasElement;
+	if (canvas) {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		initMaze(canvas);
+	}
 };
